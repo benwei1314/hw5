75421ddba66bd6c027d5b3804799f11ea6f6c29a
benwei1314 <55177022+benwei1314@users.noreply.github.com>
2019-10-11 22:36:10 -0700

    Update diamond-lines.js

--- diamond-lines.js
+++ diamond-lines.js
@@ -4,7 +4,7 @@ function setup() {
 
 function draw() {
   background(220);
-  for (var i = 0; i <= 200; i = i + 10) {  //can only do a pyramid
+  for (var i = 0; i <= 200; i = i + 10) {  //can only do a pyramid. Click on real_diamond-lines.js
     line(200 + i, i, 200 - i, i);
     
     
