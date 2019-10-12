88ff28b9c09e62cede359852cbaacc7f35a402cd
benwei1314 <55177022+benwei1314@users.noreply.github.com>
2019-10-11 19:06:56 -0700

    Create horizontal-lines.js

---
+++ horizontal-lines.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  for (var i = 50; i <= 380; i = i + 10) {
+    var startX = 20;
+    var startY = i;
+    var endX = 380;
+    var endY = i;
+    line(startX, startY, endX, endY);
+  }
+}
