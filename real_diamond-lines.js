6ce89fbce206251141f62925cf44e476e01480cc
benwei1314 <55177022+benwei1314@users.noreply.github.com>
2019-10-11 22:35:30 -0700

    Rename diamond-lines.js to real_diamond-lines.js

---
+++ real_diamond-lines.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  for (var i = 0; i <= 200; i = i + 10) { 
+    line(200 + i, i, 200 - i, i);
+    line(400 - i, 200 + i, 0 + i, 200 + i); //I did it
+    
+    
+  }
+}
